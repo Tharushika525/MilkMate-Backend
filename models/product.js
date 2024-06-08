21354c82d4e1cf88ce137fcb10cafc2946dbf6b3
Tharushika525 <hirunit770@gmail.com>
2024-06-08 17:17:02 +0530

    Add product.js

---
+++ models/product.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  productName: String,
+  productImage: String,  // Assume URL or base64 encoded image
+  purchasePrice: Number,
+  brand: String,
+  quantity: Number,
+  manufacturingDate: Date,
+  sellerName: String,
+  phoneNumber: String,
+  email: String,
+  approved: { type: Boolean, default: false },
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
