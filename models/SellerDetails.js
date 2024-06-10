f4c8e01912f1785c6d5f1664e68839cc61abe9b6
Tharushika525 <hirunit770@gmail.com>
2024-06-10 12:57:44 +0530

    Update SellerDetails.js

--- models/SellerDetails.js
+++ models/SellerDetails.js
@@ -1,4 +1,31 @@
-// models/AdminDetails.js
+
+// const mongoose = require('mongoose');
+// const Schema = mongoose.Schema;
+
+// const sellerDetailsSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: true
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     unique: true
+//   },
+//   phone: {
+//     type: String,
+//     required: true
+//   },
+//   password: {
+//     type: String,
+//     required: true
+//   }
+// });
+
+// module.exports = mongoose.model('SellerDetails', sellerDetailsSchema);
+
+
+
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -17,6 +44,18 @@ const sellerDetailsSchema = new Schema({
     required: true
   },
   password: {
+    type: String,
+    required: true
+  },
+  businessName: {
+    type: String,
+    required: true
+  },
+  city: {
+    type: String,
+    required: true
+  },
+  religion: {
     type: String,
     required: true
   }
